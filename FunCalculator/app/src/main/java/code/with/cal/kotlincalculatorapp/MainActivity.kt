961a6fe837732b1d4d0b727fb06c60debac9479b
shoryajain300 <shoryajain300@gmail.com>
2022-02-02 00:22:27 +0530

    Final push

--- FunCalculator/app/src/main/java/code/with/cal/kotlincalculatorapp/MainActivity.kt
+++ FunCalculator/app/src/main/java/code/with/cal/kotlincalculatorapp/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
+//gggg
     fun numberAction(view: View)
     {
         if(view is Button)
